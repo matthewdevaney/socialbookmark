4c3c845638302de058172bfa94277e7232ac722b
mdevaney <mbdevaney@gmail.com>
2019-01-03 20:53:45 -0600

    Chapter 4, Page 124

--- account/urls.py
+++ account/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # new user registration
+    path('register/', views.register, name='register'),
     # unused views
     # path('login/', views.user_login, name='login')
 ]
--- account/views.py
+++ account/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # local modules
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 @login_required
@@ -34,3 +34,23 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the user object
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
