4c93704c9f8184b8917ff5f94f087b2dd8d5b7bf
mdevaney <mbdevaney@gmail.com>
2019-01-03 20:10:54 -0600

    password reset feature added

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# Email settings for testing
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
