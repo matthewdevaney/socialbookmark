8b7b58aedbcb7530faedee461c3e93eb12d36068
mdevaney <mbdevaney@gmail.com>
2019-01-03 19:42:36 -0600

    password change added

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    # login logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
     # previous views
     # path('login/', views.user_login, name='login')
 ]
